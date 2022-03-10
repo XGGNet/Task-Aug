81614f08600d7ff6973ecfab83ce6da1cec1b9f5
XGGNet <chenxinli1996@gmail.com>
2022-03-10 16:51:44 +0800

    shell

--- shell/MET_testlossvis.sh
+++ shell/MET_testlossvis.sh
@@ -18,4 +18,4 @@
 CUDA_VISIBLE_DEVICES=0 python main_mame_exptestloss.py --target_domain VGH --method MET --a0 0 --a1 1 --a2 0.5 --suffix 'testlossvis_run2'
 
 # CUDA_VISIBLE_DEVICES=1 python main_mame_exptestloss.py --target_domain VGH --method MET --a0 0 --a1 0 --a2 0 --suffix 'testlossvis_run1' 
-# CUDA_VISIBLE_DEVICES=1 python main_mame_exptestloss.py --target_domain VGH --method MET --a0 0 --a1 0 --a2 0 --suffix 'testlossvis_run2' 
+# CUDA_VISIBLE_DEVICES=1 python main_mame_exptestloss.py --target_domain VGH --method MET --a0 0 --a1 0 --a2 0 --suffix 'testlossvis_run2'  
