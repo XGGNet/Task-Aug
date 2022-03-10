8775db8e14331e84028340e70bb3acc9af166cfb
XGGNet <chenxinli1996@gmail.com>
2022-03-10 16:55:39 +0800

    shell

--- shell/seg_ET.sh
+++ shell/seg_ET.sh
@@ -12,4 +12,4 @@ CUDA_VISIBLE_DEVICES=2 python main_seg_mame.py --target_domain IRCAD  --method E
 
 CUDA_VISIBLE_DEVICES=2 python main_seg_mame.py --target_domain LITS  --method ET --a0 1 --a1 0 --a2 0 --suffix 'warmup_run1'
 CUDA_VISIBLE_DEVICES=2 python main_seg_mame.py --target_domain LITS  --method ET --a0 1 --a1 0 --a2 0 --suffix 'warmup_run2'
-CUDA_VISIBLE_DEVICES=2 python main_seg_mame.py --target_domain LITS  --method ET --a0 1 --a1 0 --a2 0 --suffix 'warmup_run3'
+CUDA_VISIBLE_DEVICES=2 python main_seg_mame.py --target_domain LITS  --method ET --a0 1 --a1 0 --a2 0 --suffix 'warmup_run3' 
