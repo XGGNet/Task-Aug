0cd475d12a6fc7543a0f68494f8972d34e887608
XGGNet <chenxinli1996@gmail.com>
2022-03-10 16:50:58 +0800

    shell

--- shell/MET_sens.sh
+++ shell/MET_sens.sh
@@ -21,4 +21,4 @@ CUDA_VISIBLE_DEVICES=1 python main_mame.py --target_domain VGH  --method MET --a
 CUDA_VISIBLE_DEVICES=1 python main_mame.py --target_domain VGH  --method MET --a0 0 --a1 1.75 --a2 0 --suffix 'run2'
 
 CUDA_VISIBLE_DEVICES=1 python main_mame.py --target_domain VGH  --method MET --a0 0 --a1 2.0 --a2 0 --suffix 'run1' 
-CUDA_VISIBLE_DEVICES=1 python main_mame.py --target_domain VGH  --method MET --a0 0 --a1 2.0 --a2 0 --suffix 'run2'
+CUDA_VISIBLE_DEVICES=1 python main_mame.py --target_domain VGH  --method MET --a0 0 --a1 2.0 --a2 0 --suffix 'run2' 
