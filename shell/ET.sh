4627af5cca1df45403422fc127e9697787b6c6d1
XGGNet <chenxinli1996@gmail.com>
2022-03-10 16:49:12 +0800

    shell

--- shell/ET.sh
+++ shell/ET.sh
@@ -12,4 +12,4 @@ CUDA_VISIBLE_DEVICES=0 python main_mame.py --target_domain IHC  --method ET --a0
 
 CUDA_VISIBLE_DEVICES=0 python main_mame.py --target_domain NCH  --method ET --a0 1 --a1 0 --a2 0 --postfix 'run1'
 CUDA_VISIBLE_DEVICES=0 python main_mame.py --target_domain NCH  --method ET --a0 1 --a1 0 --a2 0 --postfix 'run2'
-CUDA_VISIBLE_DEVICES=0 python main_mame.py --target_domain NCH  --method ET --a0 1 --a1 0 --a2 0 --postfix 'run3'
+CUDA_VISIBLE_DEVICES=0 python main_mame.py --target_domain NCH  --method ET --a0 1 --a1 0 --a2 0 --postfix 'run3' 
